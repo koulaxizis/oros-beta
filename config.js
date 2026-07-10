08bfc6328542046d9477b0961558833d5afe6fe4
Christos Koulaxizis <koulaxizis@glarolykoi.net>
2026-07-11 01:16:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@
 
 var OROS_CONFIG = {
   baseHref: '/oros-beta/',
-  version: '0.7.3',
+  version: '0.7.4',
   channel: 'beta',
   domain: 'https://koulaxizis.github.io/oros-beta',
-  cacheName: 'oros-v0.7.3'
+  cacheName: 'oros-v0.7.4'
 };
 
 if (typeof window !== 'undefined') {
